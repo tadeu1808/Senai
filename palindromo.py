227291f5598797ac7b2ea82675b38c2f7e42ed75
tadeu1808 <tadeu_rocha@terra.com.br>
2025-02-15 16:55:48 -0300

    palindromo

---
+++ palindromo.py
@@ -0,0 +1,16 @@
+#verfficar se umapalavara é um palidromo
+
+#é palindromo ou não é palindromo
+#case sensitive
+
+
+print("insira a palavra:")
+palavra = str(input())
+
+if palavra == palavra[::-1]:
+   
+    print("É um Palindromo")
+    print("a palvra invertida é:", palavra[::-1])
+else:
+    print("Não é Palindromo") 
+    print("a palavra invertida é:", palavra[::-1])
---
+++ revisao_repe.py
@@ -0,0 +1,6 @@
+#tabuada do 1 até 10
+
+for x in range(1, 11, 1):
+    for y in range(11):
+        print(x,"x", y,"=",x*y)
+
