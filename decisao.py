c6c2a48a215b431f88cfbb68bd6425df80339901
tadeu_rocha <tadeu_rocha@terra.com.br>
2025-02-15 10:28:24 -0300

    aula2

---
+++ decisao.py
@@ -0,0 +1,8 @@
+ceu = "fim do mundo"
+
+if ceu == "fim do mundo"
+    print("não saio")
+elif ceu == "sol ao lado da terra":
+    print("não quero sair")
+else:
+    print("ai eu saio")
---
+++ revisao_math.py
@@ -0,0 +1,12 @@
+soma = 8+8
+print(soma)
+
+subtracao = 8-8
+print(subtracao)
+
+multiplicacao = 8*8
+print(multiplicacao)
+
+expoente = 2**3
+print(expoente)
+
